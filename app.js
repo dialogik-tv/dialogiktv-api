a9bc7fdcaedb58d7748c33e128177365389e2fde
Moritz Hesse <mh@interhesse.com>
2020-05-14 16:18:20 +0200

    Add HOST to listener

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ app.use(express.json());
 console.log('App starting...');
 
 app.use("/api/users", userRouter);
-const port = process.env.APP_PORT || 3000;
-app.listen(port, () => {
+const port = process.env.PORT || 3000;
+const host = process.env.HOST || '0.0.0.0';
+app.listen(port, host, () => {
     console.log("Server up and running on port ", port);
 });
