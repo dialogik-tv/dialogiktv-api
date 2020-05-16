e392fa7b1e5e7cbb61ca5d481f90cf451c1b298a
Moritz Hesse <mh@interhesse.com>
2020-05-16 23:54:42 +0200

    Setup CORS (allow for localhost and frontend prod host)

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
 require("dotenv").config();
+const cors = require('cors');
 const express = require("express");
 const app = express();
+
 const authRouter = require("./api/auth/auth.router");
 const userRouter = require("./api/user/user.router");
 const toolRouter = require("./api/tool/tool.router");
 
+// Setup CORS
+const allowedOrigins = [
+    'http://localhost:3000',
+    'https://dialogiktv.herokuapp.com'
+];
+app.use(cors({
+    origin: function(origin, callback){
+        // allow requests with no origin
+        // (like mobile apps or curl requests)
+        if(!origin) return callback(null, true);
+        if(allowedOrigins.indexOf(origin) === -1){
+            var msg = 'The CORS policy for this site does not ' +
+                                'allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    }
+}));
+// app.options('*', cors());
+
+// Parse bodys as JSON
 app.use(express.json());
 
 console.log('App starting...');
