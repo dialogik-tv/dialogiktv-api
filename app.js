ecb8337fb756ee88534954728d5a365c38ea7622
Moritz Hesse <mh@interhesse.com>
2020-05-17 12:24:44 +0200

    Use dotenv for CORS

--- app.js
+++ app.js
@@ -8,24 +8,8 @@ const userRouter = require("./api/user/user.router");
 const toolRouter = require("./api/tool/tool.router");
 
 // Setup CORS
-const allowedOrigins = [
-    'http://localhost:3000',
-    'https://dialogiktv.herokuapp.com'
-];
-app.use(cors({
-    origin: function(origin, callback){
-        // allow requests with no origin
-        // (like mobile apps or curl requests)
-        if(!origin) return callback(null, true);
-        if(allowedOrigins.indexOf(origin) === -1){
-            var msg = 'The CORS policy for this site does not ' +
-                                'allow access from the specified Origin.';
-            return callback(new Error(msg), false);
-        }
-        return callback(null, true);
-    }
-}));
-// app.options('*', cors());
+app.use(cors());
+app.options(process.env.CORS || '*', cors());
 
 // Parse bodys as JSON
 app.use(express.json());
@@ -38,7 +22,7 @@ app.use(apiPrefix, userRouter);
 app.use(apiPrefix, toolRouter);
 
 const port = process.env.PORT || 3000;
-const host = process.env.HOST || '0.0.0.0';
+const host = process.env.HOST || 'localhost';
 app.listen(port, host, () => {
     console.log("Server up and running on port ", port);
 });
