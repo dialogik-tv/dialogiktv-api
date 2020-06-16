b52d988345dfb82be5af5427e7bc54b5bdebfa60
Moritz Hesse <mh@interhesse.com>
2020-06-16 13:01:03 +0200

    Silence sequelize in logging

--- config/sequelize.js
+++ config/sequelize.js
@@ -21,7 +21,7 @@ module.exports = {
         database: process.env.DB_DATABASE,
         host: process.env.DB_HOST,
         dialect: process.env.DB_ENGINE,
-        // logging: false,
+        logging: false,
         pool: {
             max: 5,
             min: 0,
